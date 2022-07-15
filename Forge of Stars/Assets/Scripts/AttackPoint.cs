7e8069627dfa5e24ba5558d693125c56ce16a707
MappiCode <morten.napiralla@gmx.de>
2022-07-15 22:05:26 +0200

    Dummy and Attack-collision-detection

--- Forge of Stars/Assets/Scripts/AttackPoint.cs
+++ Forge of Stars/Assets/Scripts/AttackPoint.cs
@@ -9,6 +9,14 @@ public class AttackPoint : MonoBehaviour
 
     public Sprite[] animationSprites;
 
+    private Collider2D coll;
+    [SerializeField] private bool isAttacking = false;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     private void Update()
     {
         Vector2 aimPositionNormalized = Vector3.Normalize(aimPoint.localPosition);
@@ -16,22 +24,29 @@ public class AttackPoint : MonoBehaviour
         difference.Normalize();
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg - 90;
 
-        transform.localPosition = aimPositionNormalized * (new Vector2(1, 0.5f))/2;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        if (!isAttacking)
+        {
+            transform.localPosition = aimPositionNormalized * (new Vector2(1, 0.5f)) / 2;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        }
     }
 
-    //private IEnumerator animationCo()
-    //{
-    //    SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-    //    foreach (Sprite sprite in animationSprites)
-    //    {
-    //        spriteRenderer.sprite = sprite;
-    //        yield return new WaitForSeconds(0.1f);
-    //    }
-    //}
-
-    private void OnAttack()
+    private IEnumerator OnAttack()
     {
-        StartCoroutine(SpriteAnimator.AnimateOnceCo(GetComponent<SpriteRenderer>(), animationSprites, 0.1f));
+        isAttacking = true;
+        coll.enabled = isAttacking;
+        
+        yield return StartCoroutine(SpriteAnimator.AnimateOnceCo(GetComponent<SpriteRenderer>(), animationSprites, 0.1f));
+        
+        isAttacking = false;
+        coll.enabled = isAttacking;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isAttacking)
+            Debug.Log("Treffer");
+        else
+            Debug.Log("Angriff ins Leere");
     }
 }
